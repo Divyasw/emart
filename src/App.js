5c7b2c3d4596923dd1d870b35ed71ea0eef9ecac
Divyasw <divyasonu23@gmail.com>
2021-12-21 08:11:25 +0530

    secondproject

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
-import Home from './component/Home'
-import Navbar from './component/Navbar'
+import Home from './component/Home';
+import Navbar from './component/Navbar';
+import Products from './component/Products';
 
 function App() {
   return (
-    <> 
-      <Navbar/>
-      <Home/> 
+    <>
+      <Navbar />
+     <Home/>
+      
     </>
   );
 }
--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
+import Products from './Products';
+
 
 const Home = () => {
     return (
-        <div>
-            <div class="card bg-dark text-white">
-                <img src="/assets/bg.jpg" class="card-img" alt="Background" />
-                <div class="card-img-overlay">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">
-                        This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p class="card-text">Last updated 3 mins ago</p>
+        <div className="hero">
+            <div class="card bg-dark text-white border-0">
+                <img src="/assests/bg2.jpg" class="card-img" alt="Background"
+                    height="600px" />
+                <div class="card-img-overlay d-flex flex-column justify-content-center">
+                    <div className="container">
+                        <h5 class="card-title  display-3 fw-bolder mb-0">NEW SEASON ARRIVALS</h5>
+                        <p class="card-text text-black lead fs-2">
+                            CHECK OUT ALL THE TREND
+                        </p>
+                    </div>
                 </div>
             </div>
+            <Products/>
         </div>
-    )
-}
+    );
+};
 
 export default Home;
